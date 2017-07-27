5063c12f742f32363335f2381a155b82b306bcc8
Jingru Zhang <zhang.jingru.93@gmail.com>
2017-07-27 20:57:51 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,24 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
+
+function nestedTarget() {
+  return document.getElementById('nested').querySelector('.target')
+}
+
+function increaseRankBy(n) {
+  const lis =  document.querySelectorAll('.ranked-list')
+  lis.forEach(function(li) {li.innerHTML = li + n})
+}
+
+function deepestChild() {
+  let node = document.getElementById('grand-node')
+  let nextNode = node.children[0]
+
+  while (nextNode) {
+    node = nextNode
+    nextNode = node.children[0]
+  }
+
+  return node
+}
